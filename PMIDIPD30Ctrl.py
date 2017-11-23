473720b0e891993e71a1993267801a831ed43b8a
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-23 11:58:28 -0500

    .

--- PMIDIPD30Ctrl.py
+++ PMIDIPD30Ctrl.py
@@ -24,20 +24,17 @@ class PMIDIPD30Ctrl(ControlSurface):
     with self.component_guard():
       self._setup_transport_control()
       self._setup_mixer_control()
-    self.log_message("---------- Created PMIDIPD30Ctrl ----------")
+    self.log_message("Created PMIDIPD30Ctrl.")
 
   def _setup_transport_control(self):
-    channel = 0
-    is_momentary = True
     self._transport = TransportComponent(play_toggle_model_transform = None)
-    self._transport.set_record_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,44))
-    self._transport.set_play_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,45))
+    self._transport.set_record_button(ButtonElement(True, MIDI_CC_TYPE, 0, 44))
+    self._transport.set_play_button(ButtonElement(True, MIDI_CC_TYPE, 0, 45))
 
   def _setup_mixer_control(self):
-    channel = 0
     self._mixer = MixerComponent()
-    self._mixer.set_crossfader_control(SliderElement(MIDI_CC_TYPE, channel, 9))
+    self._mixer.set_crossfader_control(SliderElement(MIDI_CC_TYPE, 0, 9))
 
   def disconnect(self):
-    self.log_message("---------- Closed PMIDIPD30Ctrl ----------")
     ControlSurface.disconnect(self)
+    self.log_message("Closed PMIDIPD30Ctrl.")
