a4678de8982146fcec1ed962d181fb69b41eaab9
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-22 23:50:53 -0500

    .

--- PMIDIPD30Ctrl.py
+++ PMIDIPD30Ctrl.py
@@ -10,10 +10,10 @@ import Live
 
 from _Framework.ButtonElement import ButtonElement
 from _Framework.ControlSurface import ControlSurface
-from _Framework.EncoderElement import EncoderElement
+from _Framework.SliderElement import SliderElement
 from _Framework.InputControlElement import MIDI_CC_TYPE
 from _Framework.MixerComponent import MixerComponent
-from _Framework.TransportComponent import TransportComponent
+from TransportComponent import TransportComponent
 
 class PMIDIPD30Ctrl(ControlSurface):
   __module__ = __name__
@@ -32,15 +32,15 @@ class PMIDIPD30Ctrl(ControlSurface):
     self._transport = TransportComponent(play_toggle_model_transform = None)
     self._transport.set_record_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,44))
     self._transport.set_play_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,45))
-    #self._transport.set_stop_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,46))
+    self._transport.set_stop_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,46))
     self._transport.set_loop_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,49))
     self._transport.set_seek_buttons(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,48), ButtonElement(is_momentary,MIDI_CC_TYPE,channel,47))
-    #self._transport.set_punch_buttons(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,1), ButtonElement(is_momentary,MIDI_CC_TYPE,channel,2))
+    self._transport.set_punch_buttons(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,1), ButtonElement(is_momentary,MIDI_CC_TYPE,channel,2))
 
   def _setup_mixer_control(self):
     channel = 0
     self._mixer = MixerComponent()
-    self._mixer.set_crossfader_control(EncoderElement(MIDI_CC_TYPE, channel, 9, Live.MidiMap.MapMode.absolute))
+    self._mixer.set_crossfader_control(SliderElement(MIDI_CC_TYPE, channel, 9))
 
   def disconnect(self):
     self.log_message("---------- Closed PMIDIPD30Ctrl ----------")
