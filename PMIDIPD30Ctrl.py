2d09a66d66f51a79d96a9cba94749f9df698639f
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-23 00:00:36 -0500

    .

--- PMIDIPD30Ctrl.py
+++ PMIDIPD30Ctrl.py
@@ -13,7 +13,7 @@ from _Framework.ControlSurface import ControlSurface
 from _Framework.SliderElement import SliderElement
 from _Framework.InputControlElement import MIDI_CC_TYPE
 from _Framework.MixerComponent import MixerComponent
-from TransportComponent import TransportComponent
+from _Framework.TransportComponent import TransportComponent
 
 class PMIDIPD30Ctrl(ControlSurface):
   __module__ = __name__
@@ -32,10 +32,6 @@ class PMIDIPD30Ctrl(ControlSurface):
     self._transport = TransportComponent(play_toggle_model_transform = None)
     self._transport.set_record_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,44))
     self._transport.set_play_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,45))
-    self._transport.set_stop_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,46))
-    self._transport.set_loop_button(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,49))
-    self._transport.set_seek_buttons(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,48), ButtonElement(is_momentary,MIDI_CC_TYPE,channel,47))
-    self._transport.set_punch_buttons(ButtonElement(is_momentary,MIDI_CC_TYPE,channel,1), ButtonElement(is_momentary,MIDI_CC_TYPE,channel,2))
 
   def _setup_mixer_control(self):
     channel = 0
