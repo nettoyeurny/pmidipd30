e62525a800e3b6ab8ff9ff8005f7ea9be774b4a0
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-24 13:35:00 -0500

    .

--- PMIDIPD30Ctrl.py
+++ PMIDIPD30Ctrl.py
@@ -1,12 +1,8 @@
 from __future__ import with_statement
 
-import Live
-
 from _Framework.ButtonElement import ButtonElement
-from _Framework.ButtonSliderElement import ButtonSliderElement
 from _Framework.ControlSurface import ControlSurface
 from _Framework.InputControlElement import MIDI_CC_TYPE
-from _Framework.MixerComponent import MixerComponent
 from _Framework.SliderElement import SliderElement
 
 
@@ -18,56 +14,41 @@ class PMIDIPD30Ctrl(ControlSurface):
     ControlSurface.__init__(self, c_instance)
     self.__slots = self.register_slot_manager()
     with self.component_guard():
-      self.__setup_transport_control()
-      self.__setup_mixer_control()
+      self._setup()
     self.log_message("Created PMIDIPD30Ctrl.")
 
   def disconnect(self):
     ControlSurface.disconnect(self)
     self.log_message("Closed PMIDIPD30Ctrl.")
 
-  def __register_button(self, ctrl, callback):
+  def _register_button(
+      self, callback, ctrl, ch = 0, is_momentary = True, tp = MIDI_CC_TYPE):
+    if is_momentary:
+      cb = lambda v: v and callback()
+    else:
+      cb = callback
     self.__slots.register_slot(
-        ButtonElement(True, MIDI_CC_TYPE, 0, ctrl), callback, 'value')
+      ButtonElement(is_momentary, tp, ch, ctrl), cb, 'value')
 
-  def __setup_transport_control(self):
-    self.__register_button(44, self.__record)
-    self.__register_button(45, self.__start)
-    self.__register_button(46, self.__stop)
-    self.__register_button(47, self.__prev_cue)
-    self.__register_button(48, self.__next_cue)
-    self.__register_button(49, self.__toggle_cue)
+  def _register_slider(self, callback, ctrl, ch = 0, tp = MIDI_CC_TYPE):
+    self.__slots.register_slot(
+        SliderElement(tp, ch, ctrl), callback, 'value')
 
-  def __setup_mixer_control(self):
-    self._mixer = MixerComponent()
-    self._mixer.set_crossfader_control(SliderElement(MIDI_CC_TYPE, 0, 9))
-    self._secondary_mixer = MixerComponent()
-    self._secondary_mixer.set_crossfader_control(ButtonSliderElement(
-       (ButtonElement(True, MIDI_CC_TYPE, 0, 1),
-        ButtonElement(True, MIDI_CC_TYPE, 0, 2))
-    ))
+  def _setup(self):
+    self._register_button(self.__record, 44)
+    self._register_button(self.song().start_playing, 45)
+    self._register_button(self.song().stop_playing, 46)
+    self._register_button(self.song().jump_to_prev_cue, 47)
+    self._register_button(self.song().jump_to_next_cue, 48)
+    self._register_button(self.song().set_or_delete_cue, 49)
+    self._register_slider(self.__set_crossfader, 9)
+    self._register_button(lambda: self.__set_crossfader(0), 1)
+    self._register_button(lambda: self.__set_crossfader(127), 2)
 
-  def __start(self, value):
-    if not value: return
-    self.song().start_playing()
-
-  def __stop(self, value):
-    if not value: return
-    self.song().stop_playing()
-
-  def __record(self, value):
-    if not value: return
+  def __record(self):
     self.song().record_mode = not self.song().record_mode
 
-  def __prev_cue(self, value):
-    if not value: return
-    self.song().jump_to_prev_cue()
-
-  def __next_cue(self, value):
-    if not value: return
-    self.song().jump_to_next_cue()
-
-  def __toggle_cue(self, value):
-    if not value: return
-    self.song().set_or_delete_cue()
+  def __set_crossfader(self, value):
+    self.song().master_track.mixer_device.crossfader.value = max(
+        -1.0, (value - 64) / 63.0)
 
