d7e760df462d766388a0cf86f87affb5a69e307f
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-25 12:37:57 -0500

    .

--- PMIDIPD30Ctrl.py
+++ PMIDIPD30Ctrl.py
@@ -10,16 +10,16 @@ class PMIDIPD30Ctrl(SimpleControllerBase):
 
   def _setup(self):
     # Set up transport.
-    self._register_momentary_button(self.__toggle_record, 44)
-    self._register_momentary_button(self.song.start_playing, 45)
-    self._register_momentary_button(self.song.stop_playing, 46)
-    self._register_momentary_button(self.song.jump_to_prev_cue, 47)
-    self._register_momentary_button(self.song.jump_to_next_cue, 48)
-    self._register_momentary_button(self.song.set_or_delete_cue, 49)
+    self._register_trigger(self.__toggle_record, 44)
+    self._register_trigger(self.song.start_playing, 45)
+    self._register_trigger(self.song.stop_playing, 46)
+    self._register_trigger(self.song.jump_to_prev_cue, 47)
+    self._register_trigger(self.song.jump_to_next_cue, 48)
+    self._register_trigger(self.song.set_or_delete_cue, 49)
     # Set up crossfader.
     self._register_slider(self.__set_crossfader, 9)
-    self._register_momentary_button(partial(self.__set_crossfader, 0), 1)
-    self._register_momentary_button(partial(self.__set_crossfader, 127), 2)
+    self._register_trigger(partial(self.__set_crossfader, 0), 1)
+    self._register_trigger(partial(self.__set_crossfader, 127), 2)
 
   def __toggle_record(self):
     self.song.record_mode = not self.song.record_mode
--- SimpleControllerBase.py
+++ SimpleControllerBase.py
@@ -1,7 +1,6 @@
 from __future__ import with_statement
 
 from ableton.v2.control_surface.control_surface import SimpleControlSurface
-from ableton.v2.control_surface.elements.button import ButtonElement
 from ableton.v2.control_surface.elements.slider import SliderElement
 from ableton.v2.control_surface.input_control_element import MIDI_CC_TYPE
 
@@ -17,20 +16,11 @@ class SimpleControllerBase(SimpleControlSurface):
     self._c_instance.log_message(
         '(%s) %s' % (self.__class__.__name__, ' '.join(map(str, msg))))
 
-  def _register_momentary_button(
-      self, callback, ctrl, ch = 0, tp = MIDI_CC_TYPE):
-    self.register_slot(
-      ButtonElement(True, tp, ch, ctrl),
-      lambda v: v and callback(), 'value')
-
-  def _register_toggle_button(
-      self, callback, ctrl, ch = 0, tp = MIDI_CC_TYPE):
-    self.register_slot(
-      ButtonElement(False, tp, ch, ctrl), callback, 'value')
-
   def _register_slider(self, callback, ctrl, ch = 0, tp = MIDI_CC_TYPE):
-    self.register_slot(
-        SliderElement(tp, ch, ctrl), callback, 'value')
+    self.register_slot(SliderElement(tp, ch, ctrl), callback, 'value')
+
+  def _register_trigger(self, callback, ctrl, ch = 0, tp = MIDI_CC_TYPE):
+    self._register_slider(lambda v: v and callback(), ctrl, ch, tp)
 
   def _setup(self):
     raise NotImplementedError('Override _setup to set up controllers.')
