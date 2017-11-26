480ec0cf789fc48a75ec2d2c626d346f66563216
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-26 17:12:04 -0500

    .

--- SimpleControllerBase.py
+++ SimpleControllerBase.py
@@ -18,18 +18,39 @@ class SimpleControllerBase(SimpleControlSurface):
         '(%s) %s' % (self.__class__.__name__, ' '.join(map(str, msg))))
 
   def _create_midi_element(self, ctrl, ch = 0, is_cc = True):
+    """
+    Returns a MIDI element (either CC or note), e.g., for controlling lights on
+    a MIDI device. Typical usage:
+
+    e = self._create_midi_element(60, 0, is_cc = False)  # MIDI note 60, ch 0.
+    e.send_value(127, force = True)
+    """
     return InputControlElement(
         MIDI_CC_TYPE if is_cc else MIDI_NOTE_TYPE, ch, ctrl)
 
   def _register_slider(self, callback, ctrl, ch = 0, is_cc = True):
+    """
+    Attaches the given callback to a MIDI element (either CC or note). The
+    callback is invoked when a MIDI event comes in; it takes a single argument,
+    a 7-bit MIDI value. Returns the MIDI element.
+    """
     element = self._create_midi_element(ctrl, ch, is_cc)
     self.register_slot(element, callback, 'value')
     return element
 
   def _register_trigger(self, callback, ctrl, ch = 0, is_cc = True):
+    """
+    Attaches the given callback to a momentary MIDI element (either CC or note);
+    the callback takes no arguments, and it is invoked when the MIDI element
+    emits a nonzero value. Returns the MIDI element.
+    """
     return self._register_slider(lambda v: v and callback(), ctrl, ch, is_cc)
 
   def _register_listener(self, callback, subj, key):
+    """
+    Attaches a callback that takes no arguments to a property (e.g.,
+    'is_playing') of an object in Live (e.g., self.song).
+    """
     self.register_slot(Slot(subj, callback, key))
 
   def _setup(self):
