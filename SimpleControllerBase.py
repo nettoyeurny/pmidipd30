6f9f9ba82739c35dc3aa22b905756f5bc25bec6f
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-26 20:39:03 -0500

    .

--- SimpleControllerBase.py
+++ SimpleControllerBase.py
@@ -19,8 +19,8 @@ class SimpleControllerBase(SimpleControlSurface):
 
   def _create_midi_element(self, ctrl, ch = 0, is_cc = True):
     """
-    Returns a MIDI element (either CC or note), e.g., for controlling lights on
-    a MIDI device. Typical usage:
+    Returns a new MIDI element (either CC or note), e.g., for controlling lights
+    on a MIDI device. Typical usage:
 
     e = self._create_midi_element(60, 0, is_cc = False)  # MIDI note 60, ch 0.
     e.send_value(127, force = True)
@@ -30,9 +30,9 @@ class SimpleControllerBase(SimpleControlSurface):
 
   def _register_slider(self, callback, ctrl, ch = 0, is_cc = True):
     """
-    Attaches the given callback to a MIDI element (either CC or note). The
-    callback is invoked when a MIDI event comes in; it takes a single argument,
-    a 7-bit MIDI value. Returns the MIDI element.
+    Creates a new MIDI element (either CC or note) and attaches the given
+    callback to it. The callback is invoked when a MIDI event comes in; it
+    takes a single argument, a 7-bit MIDI value. Returns the MIDI element.
     """
     element = self._create_midi_element(ctrl, ch, is_cc)
     self.register_slot(element, callback, 'value')
@@ -40,9 +40,10 @@ class SimpleControllerBase(SimpleControlSurface):
 
   def _register_trigger(self, callback, ctrl, ch = 0, is_cc = True):
     """
-    Attaches the given callback to a momentary MIDI element (either CC or note);
-    the callback takes no arguments, and it is invoked when the MIDI element
-    emits a nonzero value. Returns the MIDI element.
+    Creates a new MIDI element (either CC or note) and attaches a callback to
+    it. The callback takes no arguments, and it is invoked when the MIDI element
+    emits a nonzero value. This is useful for momentary buttons. Returns the
+    MIDI element.
     """
     return self._register_slider(lambda v: v and callback(), ctrl, ch, is_cc)
 
