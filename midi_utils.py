b0233ca5c117619ea91317d53f10c3b5dd57a50a
Peter Brinkmann <peter.brinkmann@gmail.com>
2017-11-28 18:29:39 -0500

    .

--- midi_utils.py
+++ midi_utils.py
@@ -10,8 +10,8 @@ def u_to_m(x):
 
 def m_to_s(m):
   """7-bit MIDI value to signed unit range."""
-  return (m - 64) / (63 if m >= 64 else 64)
+  return (m - 64) / (63.0 if m >= 64 else 64.0)
 
 def s_to_m(x):
   """Signed unit range to 7-bit MIDI value."""
-  return int(64 + x * (63 if x >= 0 else 64)
+  return int(64 + x * (63 if x >= 0 else 64))
